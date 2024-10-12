5869886725bd0ea9b62adfdc7aa8fb28a0955db0
De1niz <erdogde4957_aehit@students.vizja.pl>
2024-10-12 22:03:30 +0200

    first commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+void checktriangle(int a, int b, int c) {
+    if (a+b>c && a+c>b && b+c>a){
+        printf("that lines can form triangle \n");
+    }
+    else {
+        printf("that lines can't form triangle \n");
+    }
+}
+
+int main()
+{
+    checktriangle(10, 10, 10);
+    checktriangle(30, 10, 10);
+    checktriangle(50, 10, 10);
+    return 0;
+}
+
